b92b3d77130e6ce44dbb416e4742ff88ebf7071c
Pedro Henrique <pedrohmogi18@gmail.com>
2026-03-14 13:01:13 -0300

    Configurando o vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'playzenha-production.up.railway.app' // Adicione o seu domínio aqui
+    ],
+    // Dica: adicione também a porta e o host para o Railway encontrar o app
+    port: 8080,
+    host: true 
+  }
 })
